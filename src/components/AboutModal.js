26bb5057afff73bcbf95ce3c153c2f3fca28193d
Amanda Smith <amanda.smith@crometrics.com>
2020-08-06 09:04:16 -0500

    Tweaked About message

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -11,7 +11,7 @@ export default function AboutModal(props) {
 				The starting row for the foreground color uses a long chain method, instead of two separate meshes, for maximum flexibility for the bottom rows.
 				If you like, you can start with two meshes instead, although the finished product might not look exactly the same as the grid you draw.
 			</p>
-			<p><strong>Please don't sell patterns created in this tool.</strong> If you post any, credit is highly appreciated!</p>
+			<p><strong>Please don't sell patterns created in this tool.</strong> You can sell the finished product though. Credit is highly appreciated!</p>
 			<p>Find a bug? Email me at <a href="mailto:crochetgeneratorcontact@gmail.com">crochetgeneratorcontact@gmail.com</a></p>
 			<p className="Modal__footer">Made in ReactJS. <a target="_blank" rel="noopener noreferrer" href="https://github.com/Mooglegirl/interlocking-crochet-generator">Source code</a></p>
 		</Modal>
